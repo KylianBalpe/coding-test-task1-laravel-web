40a1060f906b244bd4d770a15b06a7d14ffda6d0
Iqbal Pamula <balpamula@gmail.com>
2024-08-27 19:02:57 +0700

    Feat: user migration and seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'John Doe', 'email' => 'johndoe@example.com', 'password' => bcrypt('password'), 'role' => 'admin']
+        ];
+
+        DB::table('users')->insert($users);
+    }
+}
